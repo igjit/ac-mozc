ad9b04310cb3e42d14df4e145c7ede2eb19f7006
igjit <igjit1@gmail.com>
2014-04-25 21:02:45 +0900

    Add information for packaging

--- ac-mozc.el
+++ ac-mozc.el
@@ -1,5 +1,26 @@
-;; -*- coding: utf-8 -*-
-;;; ac-mozc.el --- An auto-complete source for Mozc
+;;; ac-mozc.el --- auto-complete sources for Japanese input using Mozc  -*- coding: utf-8; -*-
+
+;; Copyright (C) 2014 igjit
+
+;; Author: igjit <igjit1@gmail.com>
+;; URL: https://github.com/igjit/ac-mozc
+;; Version: 0.0.0
+;; Package-Requires: ((cl-lib "0.5") (auto-complete "1.4"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Code:
 
 (require 'cl-lib)
 (require 'mozc)
